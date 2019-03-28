a0662770bd2c5ceb1e6189c80b3726eb87e6c48f
jiraphaty <jiraphattar3@gmail.com>
2019-03-29 01:40:17 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,8 @@ clear
 
 #get web
 webreseller=$(wget -qO- https://raw.githubusercontent.com/jiraphaty/project_reseller/master/web_for_buy)
-echo "$webreseller"
+#echo "$webreseller"
+
+
+printf '\t\t################################'
+printf
